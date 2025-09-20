7ce8216d2c8cb2e39d4393f780d472279ecbc0cc
siddique <siddiquemd161@gmail.com>
2025-09-20 07:12:26 +0600

    second commit

--- src/countries/Countries.jsx
+++ src/countries/Countries.jsx
@@ -1,8 +1,25 @@
 // src/countries/Countries.jsx
-import React, { use } from 'react';
+import React, { use, useState } from 'react';
 import Country from '../country/Country';
+import './countries.css'
 
 const Countries = ({countriesPromise}) => {
+
+const [visitedcountry,setvisitedcountry]=useState([])
+const [visitedflags,setvisitedflags]=useState([])
+
+    const handalercountryvisited=(country)=>{
+        
+       const newvisitedcountry=[...visitedcountry,country]
+       console.log(newvisitedcountry)
+        setvisitedcountry(newvisitedcountry)
+    }
+    const handalervistedflags=(flags)=>{
+        
+
+        const newvisitedflags=[...visitedflags,flags]
+        setvisitedflags(newvisitedflags)
+    }
 
     const coutriesData=use(countriesPromise)
     const countries=coutriesData.countries
@@ -11,10 +28,21 @@ const Countries = ({countriesPromise}) => {
     return (
         <div>
             <h1>country {countries.length}</h1>
-            { 
-                countries.map(country=> <Country key={country.ccn3.ccn3} country={country}></Country>)
+            <h1>Visited Countries Total={visitedcountry.length} </h1>
+            <h1>visited flags total={visitedflags.length}</h1>
+           <ol >
+             {visitedcountry.map(country=><li key={country.ccn3.ccn3} >  {country.name.common}</li>)}
+           </ol>
+           <div>
+            {visitedflags.map((flags,index)=> <img key={index} src={flags} alt="" /> )}
+           </div>
+          <div className='countries'>
+              { 
+                countries.map(country=> <Country handalervistedflags={handalervistedflags}
+                    handalercountryvisited={handalercountryvisited}  key={country.ccn3.ccn3} country={country}></Country>)
             }
             
+          </div>
         </div>
     );
 };
--- src/country/Country.jsx
+++ src/country/Country.jsx
@@ -1,16 +1,40 @@
 // src/country/Country.jsx
-import React from 'react';
+import React, {  useState } from 'react';
+import  './country.css'
 
 
-const Country = ({country}) => {
+const Country = ({country,handalercountryvisited,handalervistedflags}) => {
+    
 
- console.log(country)
+         const [visited,setvisited]=useState(false)
+
+    const handaler=()=>{
+    //    if(visited){
+    //     setvisited(false)
+    //    }else{
+    //     setvisited(true)
+    //    }
+    // setvisited(visited? false:true)
+    setvisited(!visited)
+    handalercountryvisited(country)
+
+    }
+    const handalerflags=()=>{
+      handalervistedflags(country.flags.flags.png)
+
+    }
+
     return (
-        <div>
+        <div className={`flags ${visited?'country-visited':'not visited'}`   }>
             <img src={country.flags.flags.png} alt="" /> 
             <h1>name:{country.name.common}</h1>
             <p>Population : {country.population.population}</p>
+            <p>Area: {country.area.area} {country.area.area>300000? "Big Country": "Small Country"} </p>
             
+            <button onClick={handaler}>
+              {visited? 'visited':'not visited' } 
+                </button>
+                <button onClick={handalerflags}>visited flags</button>
         </div>
     );
 };
